b1e22f12c4a646af90113f8933bc8dc7ee6e72df
Maria James <mariajamesv22@gmail.com>
2023-08-12 11:53:46 +0530

    Initial setup for mobile app

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text testID="heading">Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/App/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import App from './App';
+
+describe('App', () => {
+  it('should contain the heading', () => {
+    const container = render(<App />);
+    const {getByTestId} = container;
+
+    expect(getByTestId('heading').props.children).toStrictEqual('Hello World!');
+    expect(container).toMatchSnapshot();
+  });
+});
