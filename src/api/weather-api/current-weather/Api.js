9961a88b5eda9daebc944cb8f68a6bc3efaa7019
Maria James <mariajamesv22@gmail.com>
2023-08-18 23:29:09 +0530

    Add CurrentWeather api

---
+++ src/api/weather-api/current-weather/Api.js
@@ -0,0 +1,14 @@
+import ApiService from '../../ApiService';
+import baseUrl from '../BaseUrl';
+
+class Api extends ApiService {
+    constructor({ queryParams }) {
+        super({
+            method: 'GET',
+            url: `${baseUrl}/v1/current.json`,
+            queryParams: queryParams,
+        });
+    }
+}
+
+export default Api;
