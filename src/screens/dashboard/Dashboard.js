7520d9f4d018500b145cecb2f9de08f2e5031574
Maria James <mariajamesv22@gmail.com>
2023-08-26 22:41:52 +0530

    Implement Loader in Dashboard

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -3,8 +3,19 @@ import { SafeAreaView } from 'react-native';
 import { DashboardWrapper } from './Dashboard.style';
 import SearchCitySection from '../../components/search-city-section';
 import ForecastSection from '../forecast';
+import Loader from '../../components/loader';
+import useLoading from '../../states/useLoading';
 
 const Dashboard = () => {
+    const { loading } = useLoading();
+
+    if (loading) {
+        return (
+            <SafeAreaView>
+                <Loader />
+            </SafeAreaView>
+        );
+    }
     return (
         <SafeAreaView>
             <DashboardWrapper>
--- src/screens/dashboard/Dashboard.test.js
+++ src/screens/dashboard/Dashboard.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react-native';
 import React from 'react';
 import { View as MockView } from 'react-native';
+import useLoading from '../../states/useLoading';
 import Dashboard from './Dashboard';
 
 jest.mock('../../components/search-city-section', () => {
@@ -15,8 +16,24 @@ jest.mock('../forecast', () => {
         default: () => <MockView>forecast section</MockView>,
     };
 });
+jest.mock('../../components/loader', () => {
+    return {
+        __esModule: true,
+        default: () => <MockView testID="loader">loader</MockView>,
+    };
+});
+jest.mock('../../states/useLoading');
+
 describe('Dashboard', () => {
-    it('should render Dashboard', () => {
+    it('should render Dashboard when loading state is set to false', () => {
+        useLoading.mockImplementation(() => ({ loading: false }));
+        const container = render(<Dashboard />);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should show loader when loading state is set to true', () => {
+        useLoading.mockImplementation(() => ({ loading: true }));
         const container = render(<Dashboard />);
 
         expect(container).toMatchSnapshot();
