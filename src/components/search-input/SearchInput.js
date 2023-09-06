ff16880d6984f104464b999774270e2a6553ef7b
Maria James <mariajamesv22@gmail.com>
2023-09-06 15:22:43 +0530

    Spike | Implement SearchInput initial scaffolding code

--- src/components/search-input/SearchInput.js
+++ src/components/search-input/SearchInput.js
@@ -1,43 +1,140 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { TextInput } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Modal from 'react-native-modal';
+import { Divider } from '../../commons/styles';
+import { CustomIcon } from '../../commons/visual-elements';
 import {
     SearchInputWrapper,
-    StyledTouchable,
     StyledIcon,
-    InputWrapper,
+    StyledTouchable,
 } from './SearchInput.style';
-import { CustomIcon } from '../../commons/visual-elements';
 
-const StyledSearchIcon = ({ handleOnSearchIconPress }) => (
+const StyledSearchIcon = ({ handleOnSearchIconPress, showSearchIcon }) => (
     <StyledIcon>
-        <CustomIcon
-            testID="search-icon"
-            iconName="search"
-            onPress={handleOnSearchIconPress}
-            tintColor="black"
-        />
+        <If condition={showSearchIcon}>
+            <CustomIcon
+                iconName="close"
+                size={20}
+                onPress={handleOnSearchIconPress}
+            />
+            <Else />
+            <CustomIcon
+                testID="search-icon"
+                iconName="search"
+                onPress={handleOnSearchIconPress}
+                tintColor="black"
+            />
+        </If>
     </StyledIcon>
 );
 
-const SearchInput = ({ placeholder, handleOnClick: handleOnInputBoxPress }) => {
-    const [showSearch, setShowSearch] = useState(false);
+const SearchInput = ({ placeholder }) => {
+    const [showTextInput, setShowTextInput] = useState(false);
+    const [showDropDown, setShowDropDown] = useState(false);
+
+    const [locations] = useState([
+        { name: 'abc', country: 'India', key: 1 },
+        { name: 'abc', country: 'India', key: 2 },
+        { name: 'abc', country: 'India', key: 3 },
+        { name: 'abc', country: 'India', key: 4 },
+        { name: 'abc', country: 'India', key: 5 },
+        { name: 'abc', country: 'India', key: 6 },
+    ]);
 
     const handleOnSearchIconPress = () => {
-        setShowSearch(!showSearch);
+        setShowTextInput(!showTextInput);
+    };
+
+    const handleOnChangeText = data => {
+        if (data.length === 0) {
+            setShowDropDown(false);
+        }
+        if (data.length > 3) {
+            // make api call to fetch locations, check for locations.length
+            if (locations.length !== 0) {
+                setShowDropDown(true);
+            } else {
+                setShowDropDown(false);
+            }
+        }
+    };
+
+    const handleOnDropDownItemPress = location => {
+        // make api call to fetch forecast data
     };
 
     return (
         <SearchInputWrapper>
-            <If condition={showSearch}>
-                <StyledTouchable onPress={handleOnInputBoxPress}>
-                    <InputWrapper>
+            <StyledTouchable>
+                <View style={{ width: '100%' }}>
+                    <If condition={showDropDown}>
+                        <Modal
+                            visible={showDropDown}
+                            onBackdropPress={() => setShowDropDown(false)}
+                            style={{
+                                position: 'absolute',
+                                top: 50,
+                            }}>
+                            <TextInput
+                                placeholder={placeholder}
+                                placeholderTextColor={'black'}
+                                onChangeText={handleOnChangeText}
+                            />
+                            <View
+                                style={{
+                                    borderRadius: 10,
+                                    marginTop: 12,
+                                    backgroundColor: 'white',
+                                }}>
+                                <If
+                                    condition={
+                                        showDropDown && locations.length > 0
+                                    }>
+                                    {locations.map((location, index) => {
+                                        return (
+                                            <React.Fragment key={index}>
+                                                <TouchableOpacity
+                                                    onPress={() => {
+                                                        handleOnDropDownItemPress(
+                                                            location,
+                                                        );
+                                                    }}
+                                                    style={{
+                                                        flexDirection: 'row',
+                                                        marginLeft: 10,
+                                                        alignItems: 'center',
+                                                        height: 25,
+                                                    }}>
+                                                    <CustomIcon
+                                                        iconName="pin"
+                                                        size={12}
+                                                        tintColor="black"
+                                                    />
+                                                    <Text
+                                                        style={{
+                                                            marginLeft: 5,
+                                                        }}>
+                                                        {location?.name},
+                                                        {location?.country}
+                                                    </Text>
+                                                </TouchableOpacity>
+                                                <Divider />
+                                            </React.Fragment>
+                                        );
+                                    })}
+                                </If>
+                            </View>
+                        </Modal>
+                        <Else />
                         <TextInput
                             placeholder={placeholder}
                             placeholderTextColor={'black'}
+                            onChangeText={handleOnChangeText}
                         />
-                    </InputWrapper>
-                </StyledTouchable>
-            </If>
+                    </If>
+                </View>
+            </StyledTouchable>
             <StyledSearchIcon
                 handleOnSearchIconPress={handleOnSearchIconPress}
             />
